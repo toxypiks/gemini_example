659de64531cb1d19d24052cc84ac863bc89f0095
toxypiks <HannaHinkelbein89@gmx.de>
2025-02-17 18:23:50 +0100

    Extract socket example and make request via SSL to gemini server

--- src/socket_example.c
+++ src/socket_example.c
@@ -52,44 +52,5 @@ int main(void)
         strerror(errno));
         exit(1);
     }
-
-    OpenSSL_add_all_algorithms();
-    SSL_load_error_strings();
-    SSL_CTX *ctx = SSL_CTX_new(TLS_client_method());
-
-    if (ctx == NULL) {
-        fprintf(stderr, "ERROR: could not initialize the SSL context: %s\n",
-                strerror(errno));
-        exit(1);
-    }
-
-    SSL *ssl = SSL_new(ctx);
-    SSL_set_fd(ssl, fd);
-
-    if (SSL_connect(ssl) < 0) {
-        fprintf(stderr, "ERROR: could not connect via SSL: %s\n",
-                strerror(errno));
-        exit(1);
-    }
-
-    const char *request =
-        "gemini://geminiprotocol.net/\r\n";
-
-    SSL_write(ssl, request, strlen(request));
-
-    char buffer[1024];
-    ssize_t n = SSL_read(ssl, buffer, sizeof(buffer));
-    while (n > 0) {
-        fwrite(buffer, 1, n, stdout);
-        n = SSL_read(ssl, buffer, sizeof(buffer));
-    }
-
-    SSL_set_shutdown(ssl, SSL_RECEIVED_SHUTDOWN | SSL_SENT_SHUTDOWN);
-    SSL_shutdown(ssl);
-    SSL_free(ssl);
-    SSL_CTX_free(ctx);
-
-    close(fd);
-
     return 0;
 }
